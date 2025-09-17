67c0910d6b2fb5003cc86c6ae4c81535157df241
David Lee <davidomundo@gmail.com>
2025-09-17 05:14:19 -0700

    Use enforceContrast

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,10 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { NavigationBar } from "@zoontek/react-native-navigation-bar";
 
 export default function RootLayout() {
   return (
     <>
       <StatusBar style="auto" />
-      <NavigationBar barStyle="light-content" />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen
